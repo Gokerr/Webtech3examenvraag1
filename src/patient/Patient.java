84170814b339420bd0d16c76645e5751733a5b38
Ahmet Goker <ahmetgoker02@gmail.com>
2017-01-27 11:55:16 +0100

    Opniew aanmaken van webservice

---
+++ src/patient/Patient.java
@@ -0,0 +1,11 @@
+package patient;
+
+public class Patient {
+
+public String addpatient(String datumuur, String naampatient, String naamVerpleegster, String geboortedatumn, String Allergien, String Diagnose){
+	return datumuur + " "+ naampatient + " "+ naamVerpleegster + " " + geboortedatumn + " " + Allergien + " " + Diagnose ;
+}
+	
+	
+	
+}
